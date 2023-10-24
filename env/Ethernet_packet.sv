d993fa2f814ea02f29495b7d8d71298c8d0ceaf5
hiepnguyen92 <110298554+hiepnguyen92@users.noreply.github.com>
2023-10-25 00:55:19 +0800

    Update Ethernet_packet.sv
    update for frame_type , packet_type

--- env/Ethernet_packet.sv
+++ env/Ethernet_packet.sv
@@ -9,8 +9,8 @@ class Ethernet_packet;
 	rand bit [47:0] source_address;
 	rand bit [47:0] destination_address;
 	rand byte packet_data[$];                 //queue of packet data (payload) without Source add, dest add and crc
-	bit [1:0] frame_type;			 //Ethernet type such as: IPv4, ARP, IPv6, etc.
-	bit [1:0] packet_type;			 //type of Ethernet Frame payload
+	bit [15:0] frame_type;			 //Ethernet type such as: IPv4, ARP, IPv6, etc.
+	bit [1:0] packet_type;			 //type of Ethernet Frame payload (input signal: framegen_patmode)
 	bit [31:0] packet_CRC;
 	int packet_size;                         //Total size of the packet
 	byte full_packet[$];                     //Queue of full ethernet Packet with source and destination address and CRC
@@ -20,14 +20,39 @@ class Ethernet_packet;
 		
 	function void build_packet(); 	                   //function to create build entire packet
 		int packet_size;
-		packet_size = $urandom_range(46,1500);           //minimum size of the packet should be 4 Bytes and max should be 24 Bytes
-		packet_size = (packet_size>>2)<<2;
-		for( int i = 0; i<= packet_size; i++)
-			begin
-				packet_data.push_back($urandom());
-			end
+//		packet_size = $urandom_range(46,1500);           //minimum size of the packet should be 46 Bytes and max should be 1500 Bytes
+//		packet_size = (packet_size>>2)<<2;
+		int packet_size = $urandom_range(46, 1500) inside {[46:1500]};
+		bit [1:0] packet_type = $urandom_range(0, 2);
+		
+		if (packet_type == 0) begin
+      			for (int i = 0; i < packet_size; i += 4) begin
+         			packet_data.push_back(i % 256); // Bytewise incremental
+         			packet_data.push_back((i + 1) % 256);
+         			packet_data.push_back((i + 2) % 256);
+         			packet_data.push_back((i + 3) % 256);
+      			end
+   		end
+		else if (packet_type == 1) begin
+      			byte [7:0] pattern[4] = '{8'hFF, 8'h00, 8'h55, 8'hCC}; // Constant repeating
+     			for (int i = 0; i < packet_size; i += 4) begin
+         			packet_data.push_back(pattern[i % 4]);
+         			packet_data.push_back(pattern[(i + 1) % 4]);
+         			packet_data.push_back(pattern[(i + 2) % 4]);
+         			packet_data.push_back(pattern[(i + 3) % 4]);
+      			end
+   		end
+   		else begin
+      			foreach (packet_data[i]) begin
+         			packet_data[i] = $urandom(); // Random
+      			end
+   		end
+		
 	endfunction
-	
+	function void build_frame_type();
+		bit [15:0] frame_type_values[3] = '{16'h0800, 16'h0806, 16'h86DD}; //IPv4, ARP, IPv6
+   		frame_type = frame_type_values[$urandom_range(0, 2)];
+	endfunction
 	function void build_crc();
 		packet_CRC = 32'hABCDDEAD;
 	endfunction
@@ -60,15 +85,16 @@ class Ethernet_packet;
 	endfunction
 	
 	function void post_randomize();
-		packet_size = packet_data.size() +6 +6 +2 +4; //size of data + source add + dest add + type + CRC all in bytes
-		build_address(); //fixed address
-		build_packet();
+		packet_size = packet_data.size() +6 +6 +2 +4; //size of data + source add + dest add + ethertype + CRC all in bytes
+		build_address(); //temporary using fixed address
+		build_frame_type();
+		build_packet(); 
 		build_crc();
-		for( int i = 0; i <4 ; i++)
+		for( int i = 0; i <6 ; i++)
 			begin
-				full_packet.push_back(destination_address >> i*8);     //adding destination address in the ethernet packet
+				full_packet.push_back(destination_address >> i*8);     //adding destination address in the Ethernet packet
 			end
-		for( int i = 0; i <4 ; i++)
+		for( int i = 0; i <6 ; i++)
 			begin
 				full_packet.push_back(source_address >> i*8);         //adding source address to the Ethernet packet queue
 			end
